84a09f3ca1703644de8cfec0a9cb4da7b7f9b2f7
Aniket_Khot <65432335+aniket53@users.noreply.github.com>
2021-12-15 14:36:50 +0530

    Add files via upload

---
+++ destination.js
@@ -0,0 +1,19 @@
+$(".launch-moon").click(function(){
+    // $(location).attr("href","moon.html");
+    window.location.href="moon.html";
+});
+
+$(".launch-mars").click(function(){
+    // $(location).attr("href","moon.html");
+    window.location.href="mars.html";
+});
+
+$(".launch-europa").click(function(){
+    // $(location).attr("href","moon.html");
+    window.location.href="europa.html";
+});
+
+$(".launch-titan").click(function(){
+    // $(location).attr("href","moon.html");
+    window.location.href="titan.html";
+});
